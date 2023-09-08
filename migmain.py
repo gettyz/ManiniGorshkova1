2a5a238cc0c319614888d22a1bc0fea86c6fb070
gettyz <andreym012@gmail.com>
2023-09-08 14:44:20 +0300

    добавление прямой

--- migmain.py
+++ migmain.py
@@ -2,3 +2,7 @@ from matplotlib import pyplot as plt
 plt.title('Машинки')
 plt.xlabel('Значение Х1')
 plt.ylabel('Значение X2')
+plt.xlim(0,50)
+plt.ylim(0,50)
+plt.scatter(19, 13, c= 'orange')
+plt.scatter(14, 17, c= 'blue')
