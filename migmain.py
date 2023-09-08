d5269e1bd11095fce89eb4859e15998d1b8b1bac
gettyz <andreym012@gmail.com>
2023-09-08 14:54:36 +0300

    добавление ветки версии в1

--- migmain.py
+++ migmain.py
@@ -11,3 +11,12 @@ plt.plot([19, 19],[0, 20])
 plt.plot([0,35.25],[28.2, 0])
 X = [0, 34/3]
 Y = [40/5, 0]
+plt.plot(X, Y)
+plt.arrow(0, 0, 3, 5, width= 0.2)
+plt.text(1.5, 1, 'Z(3,5)',fontsize=10)
+plt.text(20, 1, 'X1 <= 17',fontsize=10)
+plt.text(1.2, 19.5, 'X2 <= 19',fontsize=10)
+plt.text(25, 10, '4X1 + 5X2 <= 141',fontsize=10)
+plt.text(6.5, 4, 'перпендикуляр',fontsize=10)
+#display the graph
+plt.show()
