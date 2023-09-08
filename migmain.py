fce106478d1b15b56e3a1ac3593b432f265786d0
gettyz <andreym012@gmail.com>
2023-09-08 15:03:25 +0300

    добавление комментариев

--- migmain.py
+++ migmain.py
@@ -1,7 +1,9 @@
 from matplotlib import pyplot as plt
+#добавление обозначений
 plt.title('Машинки')
 plt.xlabel('Значение Х1')
 plt.ylabel('Значение X2')
+#делаем оси
 plt.xlim(0,50)
 plt.ylim(0,50)
 plt.scatter(19, 13, c= 'orange')
